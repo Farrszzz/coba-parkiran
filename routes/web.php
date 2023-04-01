9139c865869b818ef04916f23aabddbf3f7674dd
Naufal Farras <godonmiaw@gmail.com>
2023-04-01 10:41:20 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/tambah', 'App\Http\Controllers\AdminController@create');
 Route::get('user/add_user', 'App\Http\Controllers\AdminController@add_user')->name('add_user');
 Route::get('user/edit/{id}','App\Http\Controllers\AdminController@edit_user')->name('edit_user');
 Route::get('user/update','App\Http\Controllers\AdminController@edit_user_aksi')->name('edit_user_aksi');
+Route::get('user/delete', '\App\Http\Controllers\AdminController@delete'); // Menghapus data
 Route::get('/login', '\App\Http\Controllers\ParkirController@create');
 Route::get('/test', function () {
     return view('test');
